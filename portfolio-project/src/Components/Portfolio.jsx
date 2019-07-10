50c6980b8788846a4aed391256df99947206fb1d
Ryan Kegerreis <ryankegerreis@icloud.com>
2019-07-10 17:15:42 -0400

    Adding JSON file for project repository

--- portfolio-project/src/Components/Portfolio.jsx
+++ portfolio-project/src/Components/Portfolio.jsx
@@ -1,12 +1,31 @@
 import React, { Component, Fragment } from 'react'
+import portfolioProjects from './ProjectData'
 
 export default class Portfolio extends Component {
   render() {
     return (
       <Fragment>
         <div id='portfolio' className='flex-row'>
-          <h1>This is my portfolio.</h1>
-          <p>Isn't it cool?</p>
+          <div id='portfolio-header'>
+            <h1>This is my portfolio.</h1>
+          </div>
+
+          <div className='columns'>
+            <p>Isn't it cool?</p>
+            <img src={portfolioProjects[0].img} alt="" width='400px' />
+          </div>
+
+          <div className='columns'>
+            <p>Stuff</p>
+          </div>
+
+          <div className='columns'>
+            <p>More Stuff</p>
+          </div>
+
+
+
+
         </div>
       </Fragment>
     )
