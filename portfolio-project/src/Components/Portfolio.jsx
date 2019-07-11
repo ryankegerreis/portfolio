11583abcb02e8bac0d24ee21142f421c222a62e1
Ryan Kegerreis <ryankegerreis@icloud.com>
2019-07-11 17:13:11 -0400

    changed images, added link openers in new page

--- portfolio-project/src/Components/Portfolio.jsx
+++ portfolio-project/src/Components/Portfolio.jsx
@@ -12,21 +12,21 @@ export default class Portfolio extends Component {
 
           <div className='columns overlaythree'>
             <h3>{portfolioProjects[0].name}</h3>
-            <a href={portfolioProjects[0].live} title='View Site' className='pseudover v2'>
+            <a href={portfolioProjects[0].live} title='View Site' className='pseudover v2' target="_blank" rel="noopener noreferrer">
               <img src={portfolioProjects[0].img} alt={portfolioProjects[0].alt} />
             </a>
           </div>
 
           <div className='columns overlaythree'>
             <h3>{portfolioProjects[1].name}</h3>
-            <a href={portfolioProjects[1].live} title='View Site' className='pseudover v2'>
+            <a href={portfolioProjects[1].live} title='View Site' className='pseudover v2' target="_blank" rel="noopener noreferrer">
               <img src={portfolioProjects[1].img} alt={portfolioProjects[1].alt} />
             </a>
           </div>
 
           <div className='columns overlaythree'>
             <h3>{portfolioProjects[2].name}</h3>
-            <a href={portfolioProjects[2].live} title='View Site' className='pseudover v2'>
+            <a href={portfolioProjects[2].live} title='View Site' className='pseudover v2' target="_blank" rel="noopener noreferrer">
               <img src={portfolioProjects[2].img} alt={portfolioProjects[2].alt} />
             </a>
           </div>
--- portfolio-project/src/Components/Skills.jsx
+++ portfolio-project/src/Components/Skills.jsx
@@ -12,14 +12,27 @@ export default class Skills extends Component {
 
           <div className='columns'>
             <ul>
-              <li><h3>Front End</h3></li>
-              <hr />
-              <li>HTML5</li>
-              <li>CSS3</li>
-              <li>SASS</li>
-              <li>jQuery</li>
-              <li>Bootstrap</li>
-              <li>React.js</li>
+
+              <div className='left'>
+                <li><h3>Front End</h3></li>
+                <hr />
+                <img src="./images/logos/html-5.svg" alt="" srcset="" />
+                <li>HTML5</li>
+                <img src="./images/logos/css-3.svg" alt="" srcset="" />
+                <li>CSS3</li>
+                <img src="./images/logos/sass.svg" alt="" srcset="" />
+                <li>SASS</li>
+              </div>
+
+              <div className='right'>
+                <img src="./images/logos/jquery.svg" alt="" srcset="" />
+                <li>jQuery</li>
+                <img src="./images/logos/bootstrap.svg" alt="" srcset="" />
+                <li>Bootstrap</li>
+                <img src="./images/logos/react.svg" alt="" srcset="" />
+                <li>React.js</li>
+              </div>
+
             </ul>
           </div>
 
@@ -27,18 +40,20 @@ export default class Skills extends Component {
             <ul>
               <li><h3>Back End</h3></li>
               <hr />
+              <img src="./images/logos/javascript.svg" alt="" srcset="" />
               <li>Javascript</li>
+              <img src="./images/logos/nodejs.svg" alt="" srcset="" />
               <li>Node.js</li>
+              <img src="./images/logos/express.svg" alt="" srcset="" />
               <li>Express.js</li>
+              <img src="./images/logos/mongodb.svg" alt="" srcset="" />
               <li>MongoDB</li>
+              <img src="./images/logos/mongoose.svg" alt="" srcset="" />
               <li>Mongoose</li>
+              <img src="./images/logos/ajax.svg" alt="" srcset="" />
               <li>AJAX</li>
             </ul>
           </div>
-
-
-          {/* <div id='random'>
-            </div> */}
 
 
         </div>
