5073d73d73fa2df26b52333e28e81c92a14641d2
Ryan Kegerreis <ryankegerreis@icloud.com>
2019-07-12 16:44:40 -0400

    working on mobile views

--- portfolio-project/src/Components/Skills.jsx
+++ portfolio-project/src/Components/Skills.jsx
@@ -5,57 +5,72 @@ export default class Skills extends Component {
     return (
       <Fragment >
         <section id='skill-link'>
-          <div id='skills' className='flex-row'>
+          <div id='skills' className='parent'>
             <section></section>
             <div id='skills-header'>
               <h1>These are <span>some skills</span> I've collected.</h1>
             </div>
 
+            <div className='child1'>
 
-            <div className='columns'>
-              <ul>
+              <table align="center">
+                <tr><img src="./images/logos/html-5.svg" alt="" srcset="" /></tr>
+                <tr> <p>HTML5</p> </tr>
+                <tr><img src="./images/logos/css-3.svg" alt="" srcset="" /></tr>
+                <tr><p>CSS3</p></tr>
+                <tr><img src="./images/logos/sass.svg" alt="" srcset="" /></tr>
+                <tr><p>SASS</p></tr>
+              </table>
 
-                <div className='left'>
-                  <li><h3>Front End</h3></li>
-                  <hr />
-                  <img src="./images/logos/html-5.svg" alt="" srcset="" />
-                  <li>HTML5</li>
-                  <img src="./images/logos/css-3.svg" alt="" srcset="" />
-                  <li>CSS3</li>
-                  <img src="./images/logos/sass.svg" alt="" srcset="" />
-                  <li>SASS</li>
-                </div>
-
-                <div className='right'>
-                  <img src="./images/logos/jquery.svg" alt="" srcset="" />
-                  <li>jQuery</li>
-                  <img src="./images/logos/bootstrap.svg" alt="" srcset="" />
-                  <li>Bootstrap</li>
-                  <img src="./images/logos/react.svg" alt="" srcset="" />
-                  <li>React.js</li>
-                </div>
-
-              </ul>
             </div>
 
-            <div className='columns'>
-              <ul>
-                <li><h3>Back End</h3></li>
-                <hr />
-                <img src="./images/logos/javascript.svg" alt="" srcset="" />
-                <li>Javascript</li>
-                <img src="./images/logos/nodejs.svg" alt="" srcset="" />
-                <li>Node.js</li>
-                <img src="./images/logos/express.svg" alt="" srcset="" />
-                <li>Express.js</li>
-                <img src="./images/logos/mongodb.svg" alt="" srcset="" />
-                <li>MongoDB</li>
-                <img src="./images/logos/mongoose.svg" alt="" srcset="" />
-                <li>Mongoose</li>
-                <img src="./images/logos/ajax.svg" alt="" srcset="" />
-                <li>AJAX</li>
-              </ul>
+            <div className='child2'>
+
+              <table align="center">
+                <tr><img src="./images/logos/jquery.svg" alt="" srcset="" /></tr>
+                <tr> <p>HTML5</p> </tr>
+                <tr><img src="./images/logos/bootstrap.svg" alt="" srcset="" /></tr>
+                <tr><p>CSS3</p></tr>
+                <tr><img src="./images/logos/react.svg" alt="" srcset="" /></tr>
+                <tr><p>SASS</p></tr>
+              </table>
+
+
             </div>
+
+
+
+            <div className='child3'>
+
+
+              <table align="center">
+                <tr><img src="./images/logos/javascript.svg" alt="" srcset="" /></tr>
+                <tr> <p>Javascript</p> </tr>
+                <tr><img src="./images/logos/nodejs.svg" alt="" srcset="" /></tr>
+                <tr><p>Node.js</p></tr>
+                <tr><img src="./images/logos/express.svg" alt="" srcset="" /></tr>
+                <tr><p>Express.js</p></tr>
+              </table>
+
+
+
+            </div>
+
+            <div className='child4'>
+
+              <table align="center">
+                <tr><img src="./images/logos/mongodb.svg" alt="" srcset="" /></tr>
+                <tr> <p>MongoDB</p> </tr>
+                <tr><img src="./images/logos/mongoose.svg" alt="" srcset="" /></tr>
+                <tr><p>Mongoose</p></tr>
+                <tr><img src="./images/logos/ajax.svg" alt="" srcset="" /></tr>
+                <tr><p>AJAX</p></tr>
+              </table>
+
+
+
+            </div>
+
 
 
           </div>
