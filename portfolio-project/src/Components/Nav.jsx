d84de1faabff09372b7f30b28a4fa08c4744b578
Ryan Kegerreis <ryankegerreis@icloud.com>
2019-07-12 15:22:14 -0400

    added hover effects to nav bar

--- portfolio-project/src/Components/Nav.jsx
+++ portfolio-project/src/Components/Nav.jsx
@@ -8,19 +8,19 @@ export default class Nav extends Component {
           <div className="collapse navbar-collapse" >
             <ul className="navbar-nav mr-auto mx-auto">
               <li className="nav-item">
-                <a href="#home-link" className="nav-link" >Home</a >
+                <a href="#home-link" className="nav-link" id='nav-home' >Home</a >
               </li>
               <li className="nav-item">
-                <a href="#about-link" className="nav-link" >About</a >
+                <a href="#about-link" className="nav-link" id='nav-about' >About</a >
               </li>
               <li className="nav-item">
-                <a href="#skill-link" className="nav-link" >Skills</a >
+                <a href="#skill-link" className="nav-link" id='nav-skills' >Skills</a >
               </li>
               <li className="nav-item">
-                <a href="#portfolio-link" className="nav-link" >Portfolio</a >
+                <a href="#portfolio-link" className="nav-link" id='nav-port' >Portfolio</a >
               </li>
               <li className="nav-item">
-                <a href="#connect-link" className="nav-link" >Connect</a >
+                <a href="#connect-link" className="nav-link" id='nav-connect' >Connect</a >
               </li>
             </ul>
           </div>
