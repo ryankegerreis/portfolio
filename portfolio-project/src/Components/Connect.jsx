c17f19ec9221c4dd97a7352d3e7e6d79006cc1af
Ryan Kegerreis <ryankegerreis@icloud.com>
2019-07-12 15:14:16 -0400

    changed connect logos, added hover effect

--- portfolio-project/src/Components/Connect.jsx
+++ portfolio-project/src/Components/Connect.jsx
@@ -12,21 +12,21 @@ export default class Connect extends Component {
               <h1>Thanks for stopping by. <span>Let's connect.</span></h1>
             </div>
 
-            <div className='columns'>
-              <a href="https://www.linkedin.com/in/ryankegerreis/">
-                <img src={"../images/001-linkedin.png"} alt="Linkedin" className="logo-image" />
+            <div className='columns overlaythree'>
+              <a href="https://www.linkedin.com/in/ryankegerreis/" className='pseudover v2' target="_blank" rel="noopener noreferrer">
+                <img src={"../images/connect/linkedin.svg"} alt="Linkedin" className="logo-image" />
               </a>
             </div>
 
             <div className='columns'>
-              <a href="https://github.com/ryankegerreis">
-                <img src={"../images/002-github.png"} alt="Github" className="logo-image" />
+              <a href="https://github.com/ryankegerreis" className='pseudover v2' target="_blank" rel="noopener noreferrer">
+                <img src={"../images/connect/github.svg"} alt="Github" className="logo-image" />
               </a>
             </div>
 
             <div className='columns'>
-              <a href="mailto:ryankegerreis@icloud.com">
-                <img src={"../images/003-email.png"} alt="Email" className="logo-image" />
+              <a href="mailto:ryankegerreis@icloud.com" className='pseudover v2' target="_blank" rel="noopener noreferrer">
+                <img src={"../images/connect/email.svg"} alt="Email" className="logo-image" />
               </a>
             </div>
           </div>
--- portfolio-project/src/Components/Home.jsx
+++ portfolio-project/src/Components/Home.jsx
@@ -6,7 +6,7 @@ export default class Home extends Component {
       <Fragment>
         <section id='home-link'>
           <div id='home' className='flex-col'>
-            <h1>Hi <span>I'm Ryan</span>.</h1>
+            <h1>Hi <span>I'm Ryan.</span></h1>
             <p>I'm a web developer.</p>
           </div>
         </section>
