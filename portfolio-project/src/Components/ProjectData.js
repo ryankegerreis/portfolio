3fa3b89b9060a2e2280fea4b6b7ca3af4befa5da
Ryan Kegerreis <ryankegerreis@icloud.com>
2019-07-17 13:35:27 -0400

    Changed photos to svg or lower res for increased performance

--- portfolio-project/src/Components/ProjectData.js
+++ portfolio-project/src/Components/ProjectData.js
@@ -2,7 +2,7 @@ const portfolioProjects = [
   {
     id: 1,
     name: 'Chicken Run',
-    img: "./images/chicken-run.png",
+    img: "./images/chicken-run.svg",
     github: "https://github.com/ryankegerreis/platformer-game",
     live: "https://ryankegerreis.github.io/platformer-game/",
     alt: "Chicken Run picture"
@@ -10,7 +10,7 @@ const portfolioProjects = [
   {
     id: 2,
     name: 'Reinvest',
-    img: "./images/reinvest.png",
+    img: "./images/reinvest.jpg",
     github: "https://github.com/ryankegerreis/reinvest",
     live: "https://reinvest-app.herokuapp.com/",
     alt: "Reinvest picture"
@@ -18,7 +18,7 @@ const portfolioProjects = [
   {
     id: 3,
     name: 'Promise',
-    img: "./images/promise.png",
+    img: "./images/promise.svg",
     github: "https://github.com/ryankegerreis/promise",
     live: "https://promises.herokuapp.com/",
     alt: "Promise picture"
