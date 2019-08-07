c2e44fcb3f5a33f1e5d970737091ed86f9040db8
Ryan Kegerreis <ryankegerreis@icloud.com>
2019-08-07 09:20:33 +0200

    Added bing meta

--- portfolio-project/src/Components/Connect.jsx
+++ portfolio-project/src/Components/Connect.jsx
@@ -1,5 +1,6 @@
 import React, { Component, Fragment } from 'react'
 
+
 export default class Connect extends Component {
   render() {
     return (
--- portfolio-project/src/Components/Home.jsx
+++ portfolio-project/src/Components/Home.jsx
@@ -9,6 +9,7 @@ export default class Home extends Component {
         <section id='home-link'>
           <div id='home' className='flex-col'>
             <h1><ReactTypingEffect staticText="Hi," text="I'm Ryan." eraseDelay={500000000} /></h1>
+            {/* <p><ReactTypingEffect text="I'm a web developer." eraseDelay={500000000} /></p> */}
             <p>I'm a web developer.</p>
           </div>
         </section>
